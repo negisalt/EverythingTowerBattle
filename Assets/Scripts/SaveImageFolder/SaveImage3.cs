d3748c8df062fa0b251da6e37f1e5354ca26fca9
negisalt <ekus6090nek@icloud.com>
2024-12-17 16:24:07 +0900

    mainシーンの画像の表示
    画像のロードは絶望的

--- Assets/Scripts/SaveImageFolder/SaveImage3.cs
+++ Assets/Scripts/SaveImageFolder/SaveImage3.cs
@@ -1,22 +1,29 @@
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
-//using UnityEngine.Transform.localPosition;
+using UnityEngine.EventSystems;
+using SFB;
 
 public class SaveImage3 : MonoBehaviour
 {
-    //public RawImage output3;
+    public RawImage output;
+    
     public void Start()
     {
-        //output3 = texture3;
+
     }
     public void OnClick()
     {
-        Rick();
+        StartCoroutine(Rick());
     }
-    public void Rick()
+    private IEnumerator Rick()
     {
         Debug.Log(OpenImage3.ImageURL3);
-    }    
+        var loa = new WWW(OpenImage3.ImageURL3);
+        yield return loa;
+        output.texture = loa.texture;
+    }
 }
