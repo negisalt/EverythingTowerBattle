6d0dfc90d24577123bd7eb1265e839a6fcbbbb15
negisalt <ekus6090nek@icloud.com>
2024-12-18 09:58:04 +0900

    sprite取得
    あとやることはAddComponentとGameObjectととして表示すること

--- Assets/Scripts/SaveImageFolder/SaveImage3.cs
+++ Assets/Scripts/SaveImageFolder/SaveImage3.cs
@@ -9,7 +9,10 @@ using SFB;
 
 public class SaveImage3 : MonoBehaviour
 {
-    public RawImage output;
+    //public RawImage output;
+    public GameObject obj;
+    public Texture2D tex;
+    public Sprite spr;
     
     public void Start()
     {
@@ -18,12 +21,20 @@ public class SaveImage3 : MonoBehaviour
     public void OnClick()
     {
         StartCoroutine(Rick());
+        AddCom();
     }
     private IEnumerator Rick()
     {
         Debug.Log(OpenImage3.ImageURL3);
         var loa = new WWW(OpenImage3.ImageURL3);
         yield return loa;
-        output.texture = loa.texture;
+        //output.texture = loa.texture;
+        tex = loa.texture;
+        spr = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        obj.sprite = spr;
+    }
+    private void AddCom()
+    {
+        obj.AddComponent<Rigidbody2D>();
     }
 }
