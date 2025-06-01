69016f81a8844187c4f287c28f16acffdbf60723
negisalt <ekus6090nek@icloud.com>
2025-06-01 11:08:42 +0900

    newal

---
+++ Assets/Scripts/NewFolder/GeneObj.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GeneObj : MonoBehaviour
+{
+    //Collect from SaveImage.cs
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
