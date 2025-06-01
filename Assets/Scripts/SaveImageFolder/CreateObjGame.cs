fa3cc041628d3550894e40bdcc43ab7f7f3b3348
negisalt <ekus6090nek@icloud.com>
2025-06-01 17:35:11 +0900

    random(1, 2)

--- Assets/Scripts/SaveImageFolder/CreateObjGame.cs
+++ Assets/Scripts/SaveImageFolder/CreateObjGame.cs
@@ -5,27 +5,16 @@ public class CreateObjGame : MonoBehaviour
     private int count;
     private Vector2 mouPos;
     private bool leaveMouse;
+    private int randomNum;
     private GameObject obj;
     private GameObject obj1;
     private GameObject obj2;
     void Start()
     {
-
-        /*
-        obj1 = new GameObject();
-        obj1.AddComponent<SpriteRenderer>();
-        SpriteRenderer sprd = obj1.GetComponent<SpriteRenderer>();
-        sprd.sprite = SaveImage.spr1;
-        obj1.AddComponent<PolygonCollider2D>();
-        obj1.GetComponent<PolygonCollider2D>().enabled = false;
-        obj1.AddComponent<Rigidbody2D>();
-
-        Vector2 o1 = Camera.main.ScreenToWorldPoint(new Vector2(60, 300));
-        obj1.transform.position = o1;
-        */
-
         Original01();
+        Original02();
         count = 0;
+        randomNum = 1;
         leaveMouse = true;
     }
     public void FixedUpdate()
@@ -34,6 +23,14 @@ public class CreateObjGame : MonoBehaviour
         Vector2 pos = Camera.main.ScreenToWorldPoint(new Vector2(mouPos.x, 300));
         if (leaveMouse)
         {
+            if (randomNum == 1)
+            {
+                obj = Instantiate(obj1, new Vector2(0, 30), Quaternion.identity);//create object
+            }
+            else if (randomNum == 2)
+            {
+                obj = Instantiate(obj2, new Vector2(0, 30), Quaternion.identity);
+            }
             leaveMouse = false;
             Debug.Log(leaveMouse);
         }
@@ -42,15 +39,15 @@ public class CreateObjGame : MonoBehaviour
             obj.transform.position = pos;
             if (Input.GetMouseButtonDown(0))
             {
-                obj = Instantiate(obj1, new Vector2(0, 30), Quaternion.identity);//create object
                 Invoke("Timer", 0.5f);
             }
         }
     }
     private void Timer()
     {
+        randomNum = Random.Range(1, 3);
         leaveMouse = true;
-        Debug.Log(leaveMouse);
+        Debug.Log("randomNum = " + randomNum);
     }
     private void Original01()
     {
@@ -62,7 +59,7 @@ public class CreateObjGame : MonoBehaviour
         obj1.GetComponent<PolygonCollider2D>().enabled = false;
         obj1.AddComponent<Rigidbody2D>();
 
-        Vector2 o1 = Camera.main.ScreenToWorldPoint(new Vector2(60, 300));
+        Vector2 o1 = Camera.main.ScreenToWorldPoint(new Vector2(0, 300));
         obj1.transform.position = o1;
     }
     private void Original02()
@@ -70,12 +67,12 @@ public class CreateObjGame : MonoBehaviour
         obj2 = new GameObject();
         obj2.AddComponent<SpriteRenderer>();
         SpriteRenderer sprd = obj2.GetComponent<SpriteRenderer>();
-        sprd.sprite = SaveImage.spr1;
+        sprd.sprite = SaveImage.spr2;
         obj2.AddComponent<PolygonCollider2D>();
         obj2.GetComponent<PolygonCollider2D>().enabled = false;
         obj2.AddComponent<Rigidbody2D>();
 
-        Vector2 o2 = Camera.main.ScreenToWorldPoint(new Vector2(-60, 300));
+        Vector2 o2 = Camera.main.ScreenToWorldPoint(new Vector2(0, 200));
         obj2.transform.position = o2;
     }
 }
