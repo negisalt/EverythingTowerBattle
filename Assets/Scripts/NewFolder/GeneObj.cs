e23fc22f1f7572eebed38f386df99810f7032285
negisalt <ekus6090nek@icloud.com>
2025-06-01 11:18:29 +0900

    ...

--- Assets/Scripts/NewFolder/GeneObj.cs
+++ Assets/Scripts/NewFolder/GeneObj.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class GeneObj : MonoBehaviour
 {
     //Collect from SaveImage.cs
-    
+    public GameObject original;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(original, new Vector3(0.0f, 20.0f, 0.0f), Quaternion.identity);
+        }
     }
+    
 }
